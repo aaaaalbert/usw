18b6a039a46d6583dc796a07bdc2e407a9d8e861
albert <albert.rafetseder+github@univie.ac.at>
2016-10-18 23:48:00 +0200

    Handle `CreateEvent` and `DeleteEvent` too

--- my-github-events/mygithubevents.js
+++ my-github-events/mygithubevents.js
@@ -64,8 +64,23 @@ function process_events_list(events_list) {
         return_events.push([event_type, timestamp, pr_title + " --- " + comment_body, comment_html_url]);
         break;
 
+      case "CreateEvent":
+        var what = e["payload"]["ref_type"];
+        var what_name = e["payload"]["ref"];
+        event_type = "Created " + what;
+        var repo_name = e["repo"]["name"];
+        return_events.push([event_type, timestamp, repo_name + " "+ what_name, "https://github.com/" + repo_name]);
+        break;
+
+      case "DeleteEvent":
+        var what = e["payload"]["ref_type"];
+        var what_name = e["payload"]["ref"];
+        event_type = "Deleted " + what;
+        var repo_name = e["repo"]["name"];
+        return_events.push([event_type, timestamp, repo_name + " "+ what_name, "https://github.com/" + repo_name]);
+        break;
+
       default:
-        // CreateEvent, DeleteEvent, ForkEvent
         console.log("Yet-unhandled type " + e["type"]);
         return_events.push([e["type"], "ID " + e["id"], "???", "???"]);
       }
